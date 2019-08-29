c0da201861d5d271f379b52a2334083b7d35ee92
Mel BE <mbecorreo@hotmail.com>
2019-08-29 16:54:03 -0500

    Creacion de proyecto HelloWorld

---
+++ src/main/java/com/uqroo/helloworld/Welcome.java
@@ -0,0 +1,14 @@
+/*
+ * Mensaje de bienvenida a Java
+ */
+package com.uqroo.helloworld;
+
+/**
+ *
+ * @author meluser
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Java !");
+    }
+}

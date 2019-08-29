568760d75e16e520d686dc4afce8ce9a0f6208ab
mblanqueto <mbecorreo@hotmail.com>
2019-08-29 17:08:53 -0500

    Modificación de la versión y la fecha del encabezado.

--- src/main/java/com/uqroo/helloworld/Welcome.java
+++ src/main/java/com/uqroo/helloworld/Welcome.java
@@ -1,11 +1,15 @@
 /*
  * Mensaje de bienvenida a Java
+ * 
+ * 29 Ago 2019
  */
 package com.uqroo.helloworld;
 
 /**
  *
- * @author meluser
+ * @author meluserls
+ * 
+ * @version 1.0.1
  */
 public class Welcome {
     public static void main(String[] args) {
